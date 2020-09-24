3ab6576f58abad9da6a85b819680f7eef50bbbfa
KatsutoshiOtogawa <gh1ab7im@gmail.com>
2020-09-24 13:19:54 +0900

    update

--- docs/js/init.js
+++ docs/js/init.js
@@ -3,6 +3,11 @@
 
     $('.sidenav').sidenav();
     $('.parallax').parallax();
-
+    
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+// DOM読み込み前初期化処理
+$(document).ready(function(){
+  $('select').formSelect();
+});
